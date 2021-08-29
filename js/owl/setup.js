9f47591398feaab8e82dc24c286c42934af5408d
Gabrielle Tuao <gabrielle.donna2@gmail.com>
2021-08-29 00:34:24 -0300

    altering code

---
+++ js/owl/setup.js
@@ -0,0 +1,16 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false, /* posso deixar como false ou true, com true fica duas setinhas acima das bolinhas */
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
